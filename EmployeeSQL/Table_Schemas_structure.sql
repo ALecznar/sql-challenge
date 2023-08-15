13a49f6396110ec94609017c30484aded6dca8c6
Aron Lecznar <arondlecznar@gmail.com>
2023-08-14 20:08:02 -0400

    Module 9 Challenge. Contains table schemas and Data Analysis questions Queries

---
+++ EmployeeSQL/Table_Schemas_structure.sql
@@ -0,0 +1,70 @@
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(50)
+);
+
+
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(10),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY,
+    salary INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(10),
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+CREATE TABLE departments (
+    dept_no VARCHAR(10) PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+
+SELECT *
+FROM departments
+
+SELECT *
+FROM titles
+
+SELECT *
+FROM employees
+
+SELECT *
+FROM dept_emp
+
+
+SELECT *
+FROM dept_manager
+
+SELECT *
+FROM salaries
+
+
